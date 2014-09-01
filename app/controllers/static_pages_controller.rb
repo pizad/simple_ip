673e4e0fa5e973438b457410ea5c176fe8cbad57
pizad <howtolegal@gmail.com>
2014-09-01 08:31:00 -0500

    add Static_Pages

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,19 @@
+class StaticPagesController < ApplicationController
+  def about
+  end
+
+  def contact
+  end
+
+  def faq
+  end
+
+  def home
+  end
+
+  def privacy
+  end
+
+  def terms
+  end
+end
