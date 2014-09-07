696ef3a7abf064045f940e8f6a9c1007c6b2ea68
pizad <howtolegal@gmail.com>
2014-09-07 18:14:39 -0500

    user name & email

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ActiveRecord::Base
+	before_save { self.email = email.downcase }
+
+	# user name
+	validates :name,
+		presence:    true,
+		length:      { maximum: 50 }
+
+	# user email
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  	validates :email,
+  		presence: 	 true,
+  		format: 	 { with: VALID_EMAIL_REGEX },
+  		uniqueness:  { case_sensitive: false },
+
+end
