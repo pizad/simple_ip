fd15eb1cf558b988a7d151678132aba2842c1801
pizad <howtolegal@gmail.com>
2014-09-20 09:24:59 -0500

    finish userin-out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
 
+before_action :signed_in_user, only: [:edit, :update]
+before_action :correct_user,   only: [:edit, :update]
+
   def show
   	@user = User.find(params[:id])
   end
@@ -9,7 +12,6 @@ class UsersController < ApplicationController
   end
 
   def create
-  	@user = User.new(user_params)
   	if @user.save
   		sign_in @user
   		flash[:success] = "Welcome to Simple IP!"
@@ -19,6 +21,18 @@ class UsersController < ApplicationController
   	end
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private
 
     def user_params
@@ -26,7 +40,18 @@ class UsersController < ApplicationController
                                    :password_confirmation)
     end
 
-  # def sign_in
-  # end
+    # Before filters
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+      	redirect_to sign_in_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -24,12 +24,35 @@ describe "User pages" do
     end
 
     describe "with valid information" do
+      let(:user) { FactoryGirl.create(:user) }
       before do
-        fill_in "Name",         with: "Example User"
-        fill_in "Email",        with: "user@example.com"
-        fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
+        fill_in "Email",    with: user.email.upcase
+        fill_in "Password", with: user.password
+        click_button "Sign in"
       end
+
+      it { should have_title(user.name) }
+      it { should have_link('Profile',     href: user_path(user)) }
+      it { should have_link('Sign out',    href: signout_path) }
+      it { should_not have_link('Sign in', href: signin_path) }
+    end
+
+    describe "with valid information" do
+      let(:new_name)  { "New Name" }
+      let(:new_email) { "new@example.com" }
+      before do
+        fill_in "Name",             with: new_name
+        fill_in "Email",            with: new_email
+        fill_in "Password",         with: user.password
+        fill_in "Confirm Password", with: user.password
+        click_button "Save changes"
+      end
+
+      it { should have_title(new_name) }
+      it { should have_selector('div.alert.alert-success') }
+      it { should have_link('Sign out', href: signout_path) }
+      specify { expect(user.reload.name).to  eq new_name }
+      specify { expect(user.reload.email).to eq new_email }
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
@@ -61,6 +84,26 @@ describe "User pages" do
 
 	it { should have_content(user.name) }
 	it { should have_title(user.name) }
+  end
+
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before do
+      sign_in user
+      visit edit_user_path(user)
+    end
+
+    describe "page" do
+      it { should have_content("Update My Profile") }
+      it { should have_title("Edit Account") }
+      it { should have_link('change', href: 'http://gravatar.com/emails') }
+    end
+
+    describe "with invalid information" do
+      before { click_button "Save changes" }
+
+      it { should have_content('error') }
+    end
   end
 
 end
